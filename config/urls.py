e89410b507d2729c43a1de1e8e15aec9ddb53580
13wjdgk <13wjdgk@naver.com>
2021-01-18 16:03:46 +0900

    가상환경 만들기

--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from likelion import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.intro ,name='intro'),
+    path('register/', views.register, name='register'),
+    path('view/', views.view, name='view'),
+    path('check/', views.check, name='check'),
+
+
 ]
--- likelion/views.py
+++ likelion/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def intro(request):
+    return render(request,'intro.html')
+
+def view(request):
+    return render(request,'view.html')
+
+def register(request):
+    return render(request,'register.html')
+
+def check(request):
+    return render(request,'check.html')
