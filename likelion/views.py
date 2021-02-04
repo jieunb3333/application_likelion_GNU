5350ce7ab227c514f3b8c8d7e2bc19fcaa463b98
eunchae619 <eunchae619@naver.com>
2021-02-04 22:01:50 +0900

    user_id 포함해서 User 객체 작성

--- likelion/views.py
+++ likelion/views.py
@@ -1,36 +1,40 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from .models import User
 
 # Create your views here.
 def intro(request):
-    
-    return render(request,'intro.html')
+
+    return render(request, "intro.html")
+
 
 def view(request):
-    return render(request,'view.html')
+    return render(request, "view.html")
+
 
 def register(request):
-    user_id=request.GET.get('user_id')
-    print(user_id)
-    return render(request,'register.html')
+    return render(request, "register.html")
+
 
 def register_action(request):
-    inform=User()
-    inform.user_name=request.POST['name']
-    inform.user_pn=request.POST['phonenumber']
-    inform.user_grade=request.POST['grade']
-    inform.user_major=request.POST['major']
-    inform.user_q1=request.POST['q1']
-    inform.user_q2=request.POST['q2']
-    inform.user_q3=request.POST['q3']
-    inform.user_q4=request.POST['q4']
+
+    inform = User()
+    inform.user_id = request.POST["googleID"]
+    inform.user_name = request.POST["name"]
+    inform.user_pn = request.POST["phonenumber"]
+    inform.user_grade = request.POST["grade"]
+    inform.user_major = request.POST["major"]
+    inform.user_q1 = request.POST["q1"]
+    inform.user_q2 = request.POST["q2"]
+    inform.user_q3 = request.POST["q3"]
+    inform.user_q4 = request.POST["q4"]
     inform.save()
-    return redirect('/register_check/')
+    return redirect("/register_check/")
+
 
 def register_check(request):
-    inform=User.objects.filter(pk=3)
-    return render(request,'register_check.html',{'inform':inform})
+    inform = User.objects.filter(pk=3)
+    return render(request, "register_check.html", {"inform": inform})
+
 
 def check(request):
-    return render(request,'check.html')
-
+    return render(request, "check.html")
