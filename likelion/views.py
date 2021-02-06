275142b3fe8166140aedcc0a712ddb73e49f0c9a
dahae0320 <ss7ssy@naver.com>
2021-02-06 20:32:12 +0900

    ajax 수정중

--- likelion/views.py
+++ likelion/views.py
@@ -3,15 +3,6 @@ from .models import User
 
 # Create your views here.
 def intro(request):
-    if request.is_ajax():
-        print("hihi")
-        user_id = request.POST['user_id']
-        if User.objects.filter(user_id=user_id).exists():
-            print("존재함")
-            return redirect("/register_check")
-        else:
-            print("존재하지 않음. 지원서 처음 작성")
-            return redirect("/register")
     return render(request, "intro.html")
 
 
@@ -26,7 +17,6 @@ def register(request):
 def register_action(request):
     checkbox_value=request.POST["checkbox_value"]
     if checkbox_value =='no_checked':
-        print("넘어갔어")
         return redirect("/register/#check")
     else:
         inform = User()
@@ -50,6 +40,18 @@ def register_check(request):
     google_id = request.session["google_id"]
     inform = User.objects.filter(user_id=google_id)
     print(google_id)
+    if request.is_ajax():
+        print("hihi")
+        user_email = request.POST.get('user_email')
+        user_id = request.POST.get('user_id')
+        print(user_id)
+        print(user_email)
+        if User.objects.filter(user_id=user_id).exists():
+            print(user_email)
+            return redirect("/register_check")
+        else:
+            print("존재하지 않음. 지원서 처음 작성")
+            return redirect("/register")
     return render(request, "register_check.html", {"inform": inform})
 
 
